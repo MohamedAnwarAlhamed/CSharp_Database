d990e0788e3ab631f39fd8766b4129a8f341fbcf
Mohamed Anwar <76563640+MohamedAnwarAlhamed@users.noreply.github.com>
2024-05-16 18:04:46 +0300

    Add files via upload

---
+++ EF CORE Migrations/1.cs
@@ -0,0 +1,14 @@
+
+using (var context = new AppDbContext())
+{
+    foreach (var item in context.Sections.Include(x => x.Course))
+    {
+        Console.WriteLine($"Section: {item.SectionName}, " +
+            $"Course {item.Course.CourseName}");
+    }
+}
+
+Console.ReadKey();
+
+
+
