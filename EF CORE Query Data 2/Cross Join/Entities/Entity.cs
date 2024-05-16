ba2919bf9f46d04b68ff23213f3994c6c4b5a1a8
Mohamed Anwar <76563640+MohamedAnwarAlhamed@users.noreply.github.com>
2024-05-16 18:30:01 +0300

    Add files via upload

---
+++ EF CORE Query Data 2/Cross Join/Entities/Entity.cs
@@ -0,0 +1,6 @@
+﻿
+public abstract class Entity
+{
+    public int Id { get; set; }
+}
+
---
+++ EF CORE Query Data 2/Inner Join/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using (var context = new AppDbContext())
+{
+    // query syntax
+
+    //var resultQuerySyntax = (from c in context.Courses.AsNoTracking()
+    //              join s in context.Sections.AsNoTracking()
+    //                    on c.Id equals s.CourseId
+    //              select new
+    //              {
+    //                  c.CourseName,
+    //                  DateRange = s.DateRange.ToString(),
+    //                  TimeSlot = s.TimeSlot.ToString()
+    //              }).ToList();
+
+    // method syntax
+    var resultMethodSyntax =
+        context.Courses.AsNoTracking()
+        .Join(context.Sections.AsNoTracking(),
+            c => c.Id,
+            s => s.CourseId,
+            (c, s) => new
+            {
+                c.CourseName,
+                DateRange = s.DateRange.ToString(),
+                TimeSlot = s.TimeSlot.ToString()
+            }
+        ).ToList();
+
+    Console.ReadKey();
+}
+
+
