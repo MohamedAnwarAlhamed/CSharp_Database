8c236ea584733c4e8415ae00a1dd774867e3e59e
Mohamed Anwar <76563640+MohamedAnwarAlhamed@users.noreply.github.com>
2024-05-16 19:26:36 +0300

    Add files via upload

---
+++ EF CORE Transaction/Transaction/Data/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿public class AppDbContext : DbContext
+{
+    public DbSet<Account> Accounts { get; set; }
+
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+        var config = new ConfigurationBuilder().AddJsonFile("appsettings.json")
+            .Build();
+
+        var connectionString = config.GetSection("constr").Value;
+
+        optionsBuilder.UseSqlServer(connectionString);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+    }
+}
+
---
+++ EF CORE Transaction/Transaction/Entities/Book.cs
@@ -0,0 +1,8 @@
+﻿public class Account
+{
+    public string AccountNumber { get; set; }
+
+    public string AccountHolder { get; set; }
+    public decimal Balance { get; set; }
+}
+
