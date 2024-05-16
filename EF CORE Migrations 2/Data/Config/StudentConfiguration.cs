109fcdb003d61c04dd19e13d0596a655146edfaa
Mohamed Anwar <76563640+MohamedAnwarAlhamed@users.noreply.github.com>
2024-05-16 18:05:31 +0300

    Add files via upload

---
+++ EF CORE Migrations 2/Data/Config/StudentConfiguration.cs
@@ -0,0 +1,21 @@
+﻿
+public class StudentConfiguration : IEntityTypeConfiguration<Student>
+{
+    public void Configure(EntityTypeBuilder<Student> builder)
+    {
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Id).ValueGeneratedNever();
+
+        builder.Property(x => x.FName)
+            .HasColumnType("VARCHAR")
+            .HasMaxLength(50).IsRequired();
+
+        builder.Property(x => x.LName)
+        .HasColumnType("VARCHAR")
+        .HasMaxLength(50).IsRequired();
+
+
+        builder.ToTable("Students");
+    }
+}
+
---
+++ EF CORE Migrations 2/Entities/Student.cs
@@ -0,0 +1,13 @@
+﻿
+public class Student
+{
+    public int Id { get; set; }
+
+    public string? FName { get; set; }
+
+    public string? LName { get; set; }
+
+    public ICollection<Section> Sections { get; set; } = new List<Section>();
+
+}
+
