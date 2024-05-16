00de4f3b0937dba01a2f3138d80699319be23668
Mohamed Anwar <76563640+MohamedAnwarAlhamed@users.noreply.github.com>
2024-05-16 19:00:23 +0300

    Add files via upload

---
+++ EF CORE Raw Sql Query 3/Global Query Filter/Program.cs
@@ -0,0 +1,11 @@
+﻿using (var context = new AppDbContext())
+{
+    foreach (var section in context.Sections)
+    {
+        Console.WriteLine($"{section.Id}\t{section.SectionName}\t{section.DateRange}\t{section.TimeSlot}");
+    }
+}
+
+Console.ReadKey();
+
+
